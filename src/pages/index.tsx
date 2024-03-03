23681f023b948d868ff51e44ceff9387e189dd59
Gabriele Di Grazia <gabrii539@gmail.com>
2024-03-03 21:43:41 +0100

    feat: added counter UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import { useDispatch, useSelector } from "react-redux";
+//importiamo le actions
+import { increment, decrement, selectCount, set } from "@/slices/counterSlice";
+import { useRef } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +18,28 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <h1>Redux app</h1>
+        <h1>Counter</h1>
+        <p>The state of the counter is managed with Redux.</p>
+        <section style={counter}>
+
+          <button 
+          onClick={() => dispatch(increment(Number(inputRef?.current?.value)))}
+          style={{padding: '1em'}}
+          >
+            +
+          </button>
+
+          <p>{count}</p>
+
+          <button 
+          onClick={() => dispatch(decrement(Number(inputRef?.current?.value)))}
+          style={{padding: '1em'}}
+          >
+            -
+          </button>
+
+        </section>
+        <input type="text" ref={inputRef} />
       </main>
     </>
   );
