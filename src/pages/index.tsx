36b7a33bc6ad1d947b1ca079816c34d474c55c96
Gabriele Di Grazia <gabrii539@gmail.com>
2024-03-04 09:20:12 +0100

    refactor: changed title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,7 +25,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Redux App</title>
+        <title>Redux counter App</title>
         <meta name="description" content="Redux App for an assignment" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
